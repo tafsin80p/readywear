643303d8cd38f086b2d1eab7096a8c9841bfe061
tafsin80p <tafsinahmedmohim09@gmail.com>
2026-09-20 15:12:47 +0600

    fix: remove notifyButton to fix build error

--- src/components/PushNotificationManager.tsx
+++ src/components/PushNotificationManager.tsx
@@ -38,10 +38,7 @@ export default function PushNotificationManager({ toneUrl }: { toneUrl?: string 
         await OneSignal.init({
           appId: appId,
           safari_web_id: "web.onesignal.auto.6514249a-4cb8-451b-a889-88f5913c9a7f",
-          allowLocalhostAsSecureOrigin: true,
-          notifyButton: {
-            enable: true,
-          },
+          allowLocalhostAsSecureOrigin: true
         });
 
         // Add 'role: admin' tag if user is in admin dashboard
